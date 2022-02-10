f6e9a90ba5260a6fdc1f47005679ec93e4d69d2a
jessica <fuerte.jessica10@gmail.com>
2022-02-09 20:07:06 -0600

    all tests passing

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
   validateUserId,
   validateUser,
+  validatePost
 } = require('../middleware/middleware')
 
 const User = require('./users-model')
@@ -50,15 +51,26 @@ router.delete('/:id', validateUserId, async (req, res, next) => {
   }
 });
 
-router.get('/:id/posts', validateUserId, (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
+  try{
+    const result = await User.getUserPosts(req.params.id)
+    res.json(result)
+  }
+  catch(err) {
+    next(err)
+  }
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
-  // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.post('/:id/posts', validateUserId, validatePost, async (req, res, next) => {
+  try {
+    const result = await Post.insert({
+      user_id: req.params.id,
+      text: req.text
+  })
+  res.status(201).json(result)
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.use((err, req, res, next) => {
